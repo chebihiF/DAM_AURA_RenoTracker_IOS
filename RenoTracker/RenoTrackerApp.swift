1dd0edcc4998daab69daeb945937061258ed1b77
Fayçal Chebihi <chebihifaycal.it@gmail.com>
2023-09-11 13:11:48 +0100

    add Images

--- RenoTracker/RenoTrackerApp.swift
+++ RenoTracker/RenoTrackerApp.swift
@@ -9,9 +9,18 @@ import SwiftUI
 
 @main
 struct RenoTrackerApp: App {
+    
+    private var renovationProjectDataManager = RenovationProjectDataManager() // load data from JSON
+    @State private var renovationProjects = [RenovationProject]()
+    
     var body: some Scene {
         WindowGroup {
-            RenovationProjectsView(renovationProjects: RenovationProject.testData)
+            RenovationProjectsView(renovationProjects: renovationProjects)
+                .onAppear(perform: {
+                    renovationProjectDataManager.load{ renovationProjects in
+                        self.renovationProjects = renovationProjects
+                    }
+                })
         }
     }
 }
